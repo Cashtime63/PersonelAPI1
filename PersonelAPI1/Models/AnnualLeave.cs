23534ae91eec1dea604f8805ce85cd37a38a344a
Cashtime63 <cashtime6336@gmail.comn>
2025-08-25 22:28:55 +0300

    maaş hesaplama formülleri eklendi
    muhasebenden alınan bilgiler doğrultusunda hesaplama formülleri eklendi

--- PersonelAPI1/Models/AnnualLeave.cs
+++ PersonelAPI1/Models/AnnualLeave.cs
@@ -15,9 +15,30 @@ namespace PersonelAPI1.Models
 
         [Column("KullanimYili")]
         public int UsageYear { get; set; }
+        [Column("OlusturmaTarihi")]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
 
+        [Column("GuncellenmeTarihi")]
+        public DateTime? UpdatedDate { get; set; }
+
+        [Column("Aktif")]
+        public bool IsActive { get; set; } = true;
         public Employee? Employee { get; set; }
 
         public ICollection<UsedLeaveDay> UsedLeaveDays { get; set; } = new List<UsedLeaveDay>();
+        [NotMapped]
+        public int UsedDays => UsedLeaveDays?.Sum(u => u.Days) ?? 0;
+
+        [NotMapped]
+        public int RemainingDays => EntitledDays - UsedDays;
+
+        [NotMapped]
+        public decimal UsagePercentage => EntitledDays > 0 ? (decimal)UsedDays / EntitledDays * 100 : 0;
+
+        [NotMapped]
+        public bool IsExpired => UsageYear < DateTime.Now.Year;
+
+        [NotMapped]
+        public bool CanUseLeave => IsActive && !IsExpired && RemainingDays > 0;
     }
 }
